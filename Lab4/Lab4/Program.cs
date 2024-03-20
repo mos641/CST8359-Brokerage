882e5241eccdadc752e0def097d1634a77aa7b60
mostapha <80982206+mos641@users.noreply.github.com>
2024-03-20 00:28:39 -0400

    Initial commit

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,56 @@
+using Lab4.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace Lab4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddSession();
+            builder.Services.AddMvc();
+            builder.Services.AddControllersWithViews();
+            var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<MarketDbContext>(options => options.UseSqlServer(connection));
+            var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<MarketDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseSession();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# CST8359 Brokerage
+A .NET Enterprise Application for a brokerage  
+Demonstrating ASP.NET MVC Core and the Entity Framework
